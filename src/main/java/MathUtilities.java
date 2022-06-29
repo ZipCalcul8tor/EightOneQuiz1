80830d6409f52a51ad1fd1cfe23c02954d1923ed
payel <payel.nandy2580@gmail.com>
2022-06-29 10:40:11 -0400

    Add

--- src/main/java/MathUtilities.java
+++ src/main/java/MathUtilities.java
@@ -1,53 +1,67 @@
- 
+import java.util.Scanner;
 
-public class MathUtilities{
-    
-  /**
-   * Add two number together
-   * @param baseValue first number
-   * @param valueToAdd number
-   * @return the sum of the two numbers
-   */
-  public Integer add(Integer baseValue, Integer valueToAdd){
-      return null;
-  }
-
-  /**
-   * Add two number together
-   * @param baseValue first number
-   * @param valueToAdd second number
-   * @return the sum of the two numbers
-   */
-  public Double add(Double baseValue, Double valueToAdd){
-      return null;
-  }
-
-  /**
-   * Get half the value of the number
-   * @param number the number given
-   * @return the half of the number in double
-   */
-  public Double half(Integer number) {
-      return null;
-  }
-
-  /**
-   * Determine if the number is odd
-   * @param number the number given
-   * @return true if the number is odd, false if it is even
-   */
-  public Boolean isOdd(Integer number){
-      return null;
-  }
-
-
-  /**
-   * Multiply the number by itself
-   * @param number the number given
-   * @return the result of the number multiply by itself
-   */
-  public Integer square(Integer number) {
-      return null;
-  }
-
+public class MathUtilities {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number ");
+        int num1 = input.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = input.nextInt();
+        int sum = num1 + num2;
+        System.out.println("Sum is " + sum);
+    }
 }
+
+
+
+        /**
+//         * Add two number together
+//         * @param baseValue first number
+//         * @param valueToAdd number
+//         * @return the sum of the two numbers
+//         */
+//
+//        }
+//
+//        }
+//
+//        /**
+//         * Add two number together
+//         * @param baseValue first number
+//         * @param valueToAdd second number
+//         * @return the sum of the two numbers
+//         */
+//        public Double add (Double , Double valueToAdd){
+//            return null;
+//        }
+//
+//        /**
+//         * Get half the value of the number
+//         * @param number the number given
+//         * @return the half of the number in double
+//         */
+//        public Double half (Integer number){
+//            return null;
+//        }
+//
+//        /**
+//         * Determine if the number is odd
+//         * @param number the number given
+//         * @return true if the number is odd, false if it is even
+//         */
+//        public Boolean isOdd (Integer number){
+//            return null;
+//        }
+//
+//
+//        /**
+//         * Multiply the number by itself
+//         * @param number the number given
+//         * @return the result of the number multiply by itself
+//         */
+//        public Integer square (Integer number){
+//            return null;
+//        }
+//
+//    }
+//}
