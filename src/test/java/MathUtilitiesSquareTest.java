231b5e2f554d88dd23117df62142f2e937695109
payel <payel.nandy2580@gmail.com>
2022-06-29 23:11:34 -0400

    Math utility

--- src/test/java/MathUtilitiesSquareTest.java
+++ src/test/java/MathUtilitiesSquareTest.java
@@ -1,51 +1,55 @@
-//
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
-//
-//public class MathUtilitiesSquareTest {
-//    private MathUtilities utilities;
-//
-//    @Before
-//    public void setup(){
-//        utilities = new MathUtilities();
-//    }
-//
-//    @Test
-//    public void testSquareForBaseCase() {
-//        //Given
-//        int expected = 1;
-//
-//        //When
-//        int actual = utilities.square(1);
-//
-//        //Then
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void testSquareForOneDigit() {
-//        //Given
-//        int expected = 9;
-//
-//        //When
-//        int actual = utilities.square(3);
-//
-//        //Then
-//        assertEquals(expected, actual);
-//    }
-//
-//    @Test
-//    public void testSquareForTwoDigit() {
-//        //Given
-//        int expected = 121;
-//
-//        //When
-//        int actual = utilities.square(11);
-//
-//        //Then
-//        assertEquals(expected, actual);
-//    }
-//}
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MathUtilitiesSquareTest {
+    private MathUtilities utilities;
+    private Result result;
+
+    private static final double DELTA = 0.009;
+
+    @Before
+    public void setup(){
+        utilities = new MathUtilities();
+        result=new Result();
+    }
+
+    @Test
+    public void testSquareForBaseCase() {
+        //Given
+        double expected = 1;
+
+        //When
+        double actual = result.square(1);
+
+        //Then
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void testSquareForOneDigit() {
+        //Given
+        double expected = 9;
+
+        //When
+        double actual = result.square(3);
+
+        //Then
+        assertEquals(expected, actual, DELTA);
+    }
+
+    @Test
+    public void testSquareForTwoDigit() {
+        //Given
+        double expected = 121;
+
+        //When
+        double actual = result.square(11);
+
+        //Then
+        assertEquals(expected, actual, DELTA);
+    }
+}
